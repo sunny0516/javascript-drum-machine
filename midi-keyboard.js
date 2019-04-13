417fb30272718916566dcab4ec9e26e146b2b76d
argenycw <cwyan@connect.ust.hk>
2019-04-14 01:26:42 +0800

    more UI stuff

--- midi-keyboard.js
+++ midi-keyboard.js
@@ -4,6 +4,12 @@ var this_pitch;
 // The MIDI pitch number for the first (left) keyboard key
 var lowest_pitch = 60;
 
+// Theoretical: 35 - 81 inclusive
+var tracks = [
+    {id: 1, drumtype: 35, hits: [0, 0, 0, 0, 0, 0, 0, 0]},
+    {id: 2, drumtype: 38, hits: [0, 0, 0, 0, 0, 0, 0, 0]}
+]
+
 function handlePianoKeyPress(evt) {
     var this_key, this_amplitude;
 
@@ -70,19 +76,40 @@ function handlePianoKeyRelease(evt) {
     console.log("Key release event for key " + this_pitch + "!");
 }
 
+
+function handleDrumKeyPress(evt, track, btn_id, activate=true) {
+    let master_volume = parseInt($("#master-volume").val());
+    let private_volume = parseInt($("#track-" + track + "-volume").val());
+    let volume = master_volume * private_volume / 128;
+    let drumtype = tracks[track - 1].drumtype;
+    if (activate) {
+        tracks[track - 1].hits[btn_id] = 1; // store value into tracks
+        MIDI.noteOn(0, drumtype, volume); // start a MIDI note
+    }
+    else {
+        tracks[track - 1].hits[btn_id] = 0;
+    }
+    // Show a simple message in the console
+    console.log("Key press event "+ drumtype +" from track " + track + "!");  
+}
+
+function handleDrumKeyRelease(evt, track) {
+    let drumtype = tracks[track - 1].drumtype;
+    MIDI.noteOff(0, drumtype);
+}
+
 $(document).ready(function() {
     MIDI.loadPlugin({
         soundfontUrl: "./midi-js/soundfont/",
+        // You can optionally list the instruments here to preload the
+        // instruments; alternatively, each instrument will be loaded
+        // when you send the program change event
         instruments: [
-            "trumpet"
-            //
-            // You can optionally list the instruments here to preload the
-            // instruments; alternatively, each instrument will be loaded
-            // when you send the program change event
-            //
+            "trumpet", 
+            "drum"
         ],
         onprogress: function(state, progress) {
-            console.log(state, progress);
+            // console.log(state, progress);
         },
         onsuccess: function() {
             // Resuming the AudioContext when there is user interaction
@@ -96,17 +123,41 @@ $(document).ready(function() {
 
             // At this point the MIDI system is ready to be used
             MIDI.setVolume(0, 127);     // Set the volume level
-            MIDI.programChange(0, parseInt($('#program').val()));  // Get the first value of instrument select
+            // I have changed "gunshot" (127) into the drum soundfont (AG)
+            MIDI.programChange(0, 127);  // Go to the drum soundfont
 
             // Set up the event handlers for all the buttons
             $(".piano-key").on("mousedown", handlePianoKeyPress);
             $(".piano-key").on("mouseup", handlePianoKeyRelease);
 
-            //
+            $(".beat-btn").on("mousedown", function(evt) {
+                if (this.on === undefined) this.on = true;
+                else this.on = !this.on;
+                let track_id = parseInt($(this).attr("track"));
+                let btn_id = parseInt($(this).attr("code"));
+                if (this.on) {
+                  $(this).css("background-color", "rgb(53,202,197)");
+                  handleDrumKeyPress(evt, track_id, btn_id, true);
+                }
+                else {
+                  $(this).css("background-color", "#222");
+                  handleDrumKeyPress(evt, track_id, btn_id, false);
+                }
+            });
+            $(".beat-btn").on("mouseup", function(evt) {
+                let track_id = parseInt($(this).attr("track"));
+                handleDrumKeyRelease(evt, track_id);
+            });
+
             // You probably need to set up an event for your instrument change
-            //
             $('#program').change(function() {
                 MIDI.programChange(0, parseInt($(this).val()));
+            });
+            $("#midi-keyboard-tab").click(function() {
+                MIDI.programChange(0, parseInt($("#program").val()));
+            });
+            $("#drum-machine-tab").click(function() {
+                MIDI.programChange(0, 127);
             });
         }
     });
