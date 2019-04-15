67f4995df1cda00d6cb483c870fb24462a832b87
Sunny <sunnytsang1998@gmail.com>
2019-04-16 03:54:14 +0800

    Supports dynamically creating and deleting tracks now

--- midi-keyboard.js
+++ midi-keyboard.js
@@ -5,10 +5,7 @@ var this_pitch;
 var lowest_pitch = 60;
 
 // Theoretical: 35 - 81 inclusive
-var tracks = [
-    {id: 1, drumtype: 35, hits: [0, 0, 0, 0, 0, 0, 0, 0]},
-    {id: 2, drumtype: 38, hits: [0, 0, 0, 0, 0, 0, 0, 0]}
-]
+var tracks = [];
 
 function handlePianoKeyPress(evt) {
     var this_key, this_amplitude;
@@ -76,7 +73,6 @@ function handlePianoKeyRelease(evt) {
     console.log("Key release event for key " + this_pitch + "!");
 }
 
-
 function handleDrumKeyPress(evt, track, btn_id, activate=true) {
     let master_volume = parseInt($("#master-volume").val());
     let private_volume = parseInt($("#track-" + track + "-volume").val());
@@ -90,12 +86,34 @@ function handleDrumKeyPress(evt, track, btn_id, activate=true) {
         tracks[track - 1].hits[btn_id] = 0;
     }
     // Show a simple message in the console
-    console.log("Key press event "+ drumtype +" from track " + track + "!");  
+    console.log("Key press event "+ drumtype +" from track " + track + "!");
 }
 
 function handleDrumKeyRelease(evt, track) {
     let drumtype = tracks[track - 1].drumtype;
     MIDI.noteOff(0, drumtype);
+}
+
+function addTrack(name, drumtype) {
+    // Determine track_id of track that we have to add
+    let track_id = 1;
+    if (tracks.length > 0) {
+        track_id = tracks[tracks.length - 1].id + 1;
+    }
+
+    // Add the track to tracks array
+    tracks.push({id: track_id, drumtype: drumtype, hits: [0, 0, 0, 0, 0, 0, 0, 0]});
+
+    // Prepare the element to append for each row
+    let elem = '<div class="row"><div class="track-name col-2">' + name + '</div><div class="beats-panel col-7">';
+    for (let i = 0; i < 8; i++) {
+        elem += '<button class="beat-btn" type="button" track="' + track_id + '" code="' + i + '"></button> ';
+    }
+    elem += '</div><div class="col-1"><input id="track-' + track_id + '-volume" class="slider" type="range" min="0" max="100" value="100" /></div>';
+    elem += '<div class="col-1"><input id="track-' + track_id + '-pitch" class="slider" type="range" min="0" max="100" value="50" /></div>';
+    elem += '<div class="col-1"><button type="button" track="' + track_id + '" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div>';
+
+    $("#track-area").append(elem);
 }
 
 $(document).ready(function() {
@@ -105,7 +123,7 @@ $(document).ready(function() {
         // instruments; alternatively, each instrument will be loaded
         // when you send the program change event
         instruments: [
-            "trumpet", 
+            "trumpet",
             "drum"
         ],
         onprogress: function(state, progress) {
@@ -126,28 +144,46 @@ $(document).ready(function() {
             // I have changed "gunshot" (127) into the drum soundfont (AG)
             MIDI.programChange(0, 127);  // Go to the drum soundfont
 
+            // Add the two test channels
+            addTrack("Example 1", 35);
+            addTrack("Example 2", 38);
+
             // Set up the event handlers for all the buttons
             $(".piano-key").on("mousedown", handlePianoKeyPress);
             $(".piano-key").on("mouseup", handlePianoKeyRelease);
 
-            $(".beat-btn").on("mousedown", function(evt) {
-                if (this.on === undefined) this.on = true;
-                else this.on = !this.on;
+            $(document.body).on("mousedown", ".beat-btn", function(evt) {
                 let track_id = parseInt($(this).attr("track"));
                 let btn_id = parseInt($(this).attr("code"));
+                this.on = !this.on;
+
                 if (this.on) {
-                  $(this).css("background-color", "rgb(53,202,197)");
-                  handleDrumKeyPress(evt, track_id, btn_id, true);
+                    $(this).css("background-color", "rgb(53,202,197)");
+                } else {
+                    $(this).css("background-color", "#222");
                 }
-                else {
-                  $(this).css("background-color", "#222");
-                  handleDrumKeyPress(evt, track_id, btn_id, false);
-                }
+                handleDrumKeyPress(evt, track_id, btn_id, this.on);
             });
-            $(".beat-btn").on("mouseup", function(evt) {
+            $(document.body).on("mouseup", ".beat-btn", function(evt) {
                 let track_id = parseInt($(this).attr("track"));
                 handleDrumKeyRelease(evt, track_id);
             });
+
+            // Delete button for each track.
+            $(document.body).on("click", ".close", function() {
+                let track_id = parseInt($(this).attr("track"));
+                $(this).parent().parent().remove();
+                tracks = tracks.filter(function(elem) {
+                    return elem.id != track_id;
+                });
+            });
+
+            // Add track button.
+            $("#track-add").click(function() {
+                let name = $("#track-add-name").val();
+                let drumtype = parseInt($("#track-add-drumtype").val());
+                addTrack(name, drumtype);
+            })
 
             // You probably need to set up an event for your instrument change
             $('#program').change(function() {
